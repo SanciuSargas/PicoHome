77deeeb2c50eeae085cd98b2215a8e8f8d2a116f
SanciuSargas <dziastinianas69@gmail.com>
2025-07-14 19:14:08 +0300

    Implemented BasicOutputDevice

--- lib/BasicOutputDevice/BasicOutputDevice.cpp
+++ lib/BasicOutputDevice/BasicOutputDevice.cpp
@@ -6,35 +6,53 @@
 
 #include "BasicOutputDevice.h"
 
-BasicOutputDevice::BasicOutputDevice(AbstractPin* pinNumber) : BasicOutputInterface(), Subscriber()
+BasicOutputDevice::BasicOutputDevice(AbstractPin* pinObject, bool highTrigger, char* chanelPathForMQTT, CommunicationInterface* communicationClient) : BasicOutputInterface(), Subscriber()
 {
-    this->pinNumber = pinNumber;
+    this->pinObject = pinObject;
+    this->highTrigger = highTrigger;
+    strcpy(this->chanelPathForMQTT, chanelPathForMQTT);
+    this->communicationClient = communicationClient;
 }
 
 void BasicOutputDevice::initialize()
 {
-
+    pinObject->setPinMode(OUTPUT);
+    pinObject->digitalWrite(!highTrigger);
+    communicationClient->publish(chanelPathForMQTT, reinterpret_cast<uint8_t*>(&currentState));
 }
 void BasicOutputDevice::turnOn()
 {
-
+    pinObject->digitalWrite(highTrigger);
+    currentState = highTrigger;
+    communicationClient->publish(chanelPathForMQTT, reinterpret_cast<uint8_t*>(&currentState));
 }
 void BasicOutputDevice::turnOff()
 {
-
+    pinObject->digitalWrite(!highTrigger);
+    currentState = !highTrigger;
+    communicationClient->publish(chanelPathForMQTT, reinterpret_cast<uint8_t*>(&currentState));
 }
 void BasicOutputDevice::toggle()
 {
-
+    if(currentState == highTrigger)
+    {
+        pinObject->digitalWrite(!highTrigger);
+        currentState = !highTrigger;
+    } else
+    {
+        pinObject->digitalWrite(highTrigger);
+        currentState = highTrigger;
+    }
+    communicationClient->publish(chanelPathForMQTT, reinterpret_cast<uint8_t*>(&currentState));
 }
-void BasicOutputDevice::getState()
+int BasicOutputDevice::getState()
 {
-
+    return currentState;
 }
 
 void BasicOutputDevice::callback()
 {
-
+    // TODO: still have to think about this.
 }
 
 
--- lib/BasicOutputDevice/BasicOutputDevice.h
+++ lib/BasicOutputDevice/BasicOutputDevice.h
@@ -5,22 +5,29 @@
 
 #ifndef BASICOUTPUTDEVICE_H
 #define BASICOUTPUTDEVICE_H
+#include <Arduino.h>
+
+#include "CommunicationInterface.h"
 #include "../AbstractPin/AbstractPin.h"
 #include "../Subscriber/Subscriber.h"
 #include "../BasicOutputInterface/BasicOutputInterface.h"
 
 class BasicOutputDevice : public BasicOutputInterface, public Subscriber
 {
-    public:
-        BasicOutputDevice(AbstractPin* pinNumber);
-        void initialize() override;
-        void turnOn() override;
-        void turnOff() override;
-        void toggle() override;
-        void getState() override;
-        void callback() override;
+public:
+    BasicOutputDevice(AbstractPin* pinObject, bool highTrigger, char* chanelPathForMQTT, CommunicationInterface* communicationClient);
+    void initialize() override;
+    void turnOn() override;
+    void turnOff() override;
+    void toggle() override;
+    int getState() override;
+    void callback() override;
 private:
-        AbstractPin* pinNumber;
+    AbstractPin* pinObject;
+    bool highTrigger;
+    CommunicationInterface* communicationClient;
+    char* chanelPathForMQTT;
+    int currentState;
 };
 
 #endif //BASICOUTPUTDEVICE_H
