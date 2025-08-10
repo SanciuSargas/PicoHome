82cfbda2aa7d30a8580facca6b7774279d8dc013
SanciuSargas <dziastinianas69@gmail.com>
2025-08-10 20:59:34 +0000

    Implemented ExpanderPin class to encapsulate mcp23017 pin extender

--- lib/ExpanderPin/ExpanderPin.cpp
+++ lib/ExpanderPin/ExpanderPin.cpp
@@ -4,3 +4,47 @@
  */
 
 #include "ExpanderPin.h"
+
+ExpanderPin::ExpanderPin(int pinNumber, Adafruit_MCP23X17* mcp)
+{
+    this->pinNumber = pinNumber;
+    this->mcp = mcp;
+}
+
+void ExpanderPin::attachInterrupt(voidFuncPtr callback, int mode)
+{
+    // TODO: add a warning that interrupts for expanderPin is handled through MCPInterruptHandler
+    Serial.println("Interrupts for expanderPin are handled through MCPInterruptHandler");
+}
+
+void ExpanderPin::pinMode(int mode)
+{
+    ArduinoPinMode arduinoMode = static_cast<ArduinoPinMode>(mode);
+    mcp->pinMode(pinNumber, arduinoMode);
+}
+
+void ExpanderPin::digitalWrite(int value)
+{
+    PinStatus arduinoValue = static_cast<PinStatus>(value);
+    mcp->digitalWrite(pinNumber, arduinoValue);
+}
+
+int ExpanderPin::digitalRead()
+{
+    return mcp->digitalRead(pinNumber);
+}
+
+
+// i2c expender doesn't have ADC capability
+void ExpanderPin::analogWrite(int value)
+{
+    if (value >= 1) value = 1;
+    mcp->digitalWrite(pinNumber, value);
+}
+
+// i2c expender doesn't have ADC capability
+int ExpanderPin::analogRead()
+{
+    if (mcp->digitalRead(pinNumber) == 1) return 4095;
+    else return 0;
+}
--- lib/ExpanderPin/ExpanderPin.h
+++ lib/ExpanderPin/ExpanderPin.h
@@ -5,10 +5,21 @@
 
 #ifndef EXPANDERPIN_H
 #define EXPANDERPIN_H
+#include "Adafruit_MCP23X17.h"
 #include "../AbstractPin/AbstractPin.h"
 
 class ExpanderPin : public AbstractPin
 {
-
+public:
+    ExpanderPin(int pinNumber, Adafruit_MCP23X17* mcp);
+    void attachInterrupt(voidFuncPtr callback, int mode) override;
+    void pinMode(int mode) override;
+    void digitalWrite(int value) override;
+    int digitalRead() override;
+    void analogWrite(int value) override;
+    int analogRead() override;
+private:
+    int pinNumber;
+    Adafruit_MCP23X17* mcp;
 };
 #endif //EXPANDERPIN_H
